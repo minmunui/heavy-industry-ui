ff2d3c511c394cda58f971b2bfc4b83156cabfeb
minmunui <ehdwls1638@gmail.com>
2024-10-25 17:24:58 +0900

    feat : add detail view

--- src/views/detail/DetailView.vue
+++ src/views/detail/DetailView.vue
@@ -3,8 +3,12 @@ import api from '@/api/api.js'
 
 export default {
   i18n: {
-    ko: { 'Loading...': '불러오는 중' },
-    cn: { 'Loading...': '加载中' },
+    ko: { 'Loading...': '불러오는 중',
+      'File Name': '파일 이름',
+    },
+    cn: { 'Loading...': '加载中',
+      'File Name': '文件名',
+    },
   },
   name: 'detail-panel',
   components: {},
@@ -31,7 +35,8 @@ export default {
   methods: {
     getImage() {
       api.getStitchedImage(this.$props.fileName, this.$props.step).then((res) => {
-        this.image = res.data
+        console.log("res", res)
+        this.image = res
       }).catch((err) => {
         this.error = err
       }).finally(() => {
@@ -44,11 +49,41 @@ export default {
 
 <template>
   <div class="detail-view">
+    <header class="image-info">
+      <span class="file-name">{{this.$t('File Name')}} : {{this.$props.fileName}}</span><span class="file-step">{{this.$t('Step')}} : {{this.$props.step}}</span>
+    </header>
     <div v-if="isLoading">{{ $t('Loading...') }}</div>
-    {{ this.$props.fileName }}
+    <div v-else-if="error">{{ error }}</div>
+    <div v-else class="image-viewer">
+      <img :src="image" alt="stitched image" />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.detail-view {
+  width: 100%;
 
+  .image-info {
+    top:0;
+    position: fixed;
+    width: 100%;
+    display: flex;
+    padding: 0.25rem;
+    gap: 1rem;
+    span {
+      white-space: nowrap;
+      color: var(--pico-h1-color);
+    }
+  }
+}
+
+.image-viewer {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: scroll;
+}
 </style>
