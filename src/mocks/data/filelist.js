ca7f4afb3f890aceebccff3d384f8045423bd6ee
minmunui <ehdwls1638@gmail.com>
2024-10-28 00:18:04 +0900

    refactor : refactor data

---
+++ src/mocks/data/filelist.js
@@ -0,0 +1,160 @@
+import { DATA_STATUS } from '@/utils/constants.js'
+
+export const M_FILE_LIST = [
+  {
+    name: '20241225103024_deokgok_1',
+    size: 129,
+    time: '2024-09-25 10:30:24',
+    status_1: {
+      status: DATA_STATUS.DONE,
+      data: {}
+    },
+    status_2: {
+      status: DATA_STATUS.ONPROGRESS,
+      data: { startedAt: '2024-09-25 10:30:24' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_2',
+    size: 129,
+    time: '2024-09-25 10:33:24',
+    status_1: {
+      status: DATA_STATUS.UPLOADING,
+      data: { startedAt: '2024-09-25 10:30:00' }
+    },
+    status_2: {
+      status: DATA_STATUS.READY,
+      data: { uploadedAt: '2024-09-25' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_2',
+    size: 129,
+    time: '2024-09-25 10:33:24',
+    status_1: {
+      status: DATA_STATUS.READY,
+      data: { error: 'Unstitchable' }
+    },
+    status_2: {
+      status: DATA_STATUS.READY,
+      data: { uploadedAt: '2024-09-25' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_1',
+    size: 129,
+    time: '2024-09-25 10:30:24',
+    status_1: {
+      status: DATA_STATUS.DONE,
+      data: {}
+    },
+    status_2: {
+      status: DATA_STATUS.ONPROGRESS,
+      data: { startedAt: '2024-09-25 10:30:24' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_2',
+    size: 129,
+    time: '2024-09-25 10:33:24',
+    status_1: {
+      status: DATA_STATUS.UPLOADING,
+      data: { startedAt: '2024-09-25 10:30:00' }
+    },
+    status_2: {
+      status: DATA_STATUS.READY,
+      data: { uploadedAt: '2024-09-25' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_2',
+    size: 129,
+    time: '2024-09-25 10:33:24',
+    status_1: {
+      status: DATA_STATUS.ERROR,
+      data: { errorLog: 'Unstitchable' }
+    },
+    status_2: {
+      status: DATA_STATUS.READY,
+      data: { uploadedAt: '2024-09-25' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_1',
+    size: 129,
+    time: '2024-09-25 10:30:24',
+    status_1: {
+      status: DATA_STATUS.DONE,
+      data: {}
+    },
+    status_2: {
+      status: DATA_STATUS.ONPROGRESS,
+      data: { startedAt: '2024-09-25 10:30:24' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_2',
+    size: 129,
+    time: '2024-09-25 10:33:24',
+    status_1: {
+      status: DATA_STATUS.UPLOADING,
+      data: { startedAt: '2024-09-25 10:30:00' }
+    },
+    status_2: {
+      status: DATA_STATUS.READY,
+      data: { uploadedAt: '2024-09-25' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_2',
+    size: 129,
+    time: '2024-09-25 10:33:24',
+    status_1: {
+      status: DATA_STATUS.ERROR,
+      data: { error: 'Unstitchable' }
+    },
+    status_2: {
+      status: DATA_STATUS.READY,
+      data: { uploadedAt: '2024-09-25' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_1',
+    size: 129,
+    time: '2024-09-25 10:30:24',
+    status_1: {
+      status: DATA_STATUS.DONE,
+      data: {}
+    },
+    status_2: {
+      status: DATA_STATUS.ONPROGRESS,
+      data: { startedAt: '2024-09-25 10:30:24' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_2',
+    size: 129,
+    time: '2024-09-25 10:33:24',
+    status_1: {
+      status: DATA_STATUS.UPLOADING,
+      data: { startedAt: '2024-09-25 10:30:00' }
+    },
+    status_2: {
+      status: DATA_STATUS.READY,
+      data: { uploadedAt: '2024-09-25' }
+    }
+  },
+  {
+    name: '20241225103024_deokgok_2',
+    size: 129,
+    time: '2024-09-25 10:33:24',
+    status_1: {
+      status: DATA_STATUS.ERROR,
+      data: { error: 'Unstitchable' }
+    },
+    status_2: {
+      status: DATA_STATUS.READY,
+      data: { uploadedAt: '2024-09-25' }
+    }
+  }
+]
